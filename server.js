51dbbf93d3de831950ec15e5743a8a0f549e0435
Jared <jaredterrospin@gmail.com>
2022-08-02 00:43:24 -0400

    added ports to listen to

--- server.js
+++ server.js
@@ -86,4 +86,4 @@ app
         })
     })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`))
